18b6a8161b6437eea4ea626c1bd904ff59acd4f9
NSS <dwindsor@networksecurityservicesllc.com>
2011-11-14 14:55:13 -0500

    Add tests directory; add test for config-file mode

---
+++ tests/config_file_test.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+#
+# Test carver's ability to process configuration files.
+# This test exercises every carver input plugin.
+#
+# Usage: config_file_test.py <config file> <data file>
+#
+
+import config;
+import carver_lib;
+import evt_plugin;
+import sys;
+
+def verify_results(headers, records, name):
+    """Verify the results of this test. Returns True if the test has \
+    passed, False otherwise."""
+    # Determine which kind of records we have
+    if name == evt_plugin.name:
+
+    else:
+        print "[FAILED] Internal error."
+        return False
+
+    if len(records) != 4:
+        print "[FAILED] Incorrect number of records found."
+        return False
+
+
+if len(sys.argv) != 3:
+    print "Usage: %s <config file> <data file>" % sys.argv[0]
+    sys.exit(1)
+
+cfg_path = sys.argv[1]
+data_path = sys.argv[2]
+
+cf = config.ConfigFile(cfg_path)
+
+# Get a list of supported plugins
+for name in carver_lib.getSupportedTypes():
+    plugin = carver_lib.getPlugin(name)
+    (headers, records) = plugin.searchFile(data_path, cfg_path, verbose=True)
+    verify_results(headers, records, name)
+
